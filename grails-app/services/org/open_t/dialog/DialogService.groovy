5aeedab0dc7ee595120c09046ce6e3ba1e1241c3
Joost Horward <joost@open-t.nl>
2011-12-29 23:13:49 +0100

    - Added comments - Added manageJoin method

--- grails-app/services/org/open_t/dialog/DialogService.groovy
+++ grails-app/services/org/open_t/dialog/DialogService.groovy
@@ -31,9 +31,18 @@ class DialogService {
 	def sessionFactory
 	
     boolean transactional = true
-      
+    
+	/**
+	* Prepares data for an edit action. The data consists of a Domain object instance with default values that is wrapped in a map
+	* Based on the id in params the instance is retrieved or a fresh instance is created 
+	* @param domainClass The domain class to be used
+	* @param params The parameters from the http request 
+	* @return a map that is ready to be rendered as a JSON message
+	*/
+
     def edit(domainClass,params) {
-		def defaultDomainClass = new DefaultGrailsDomainClass( domainClass )		
+		def defaultDomainClass = new DefaultGrailsDomainClass( domainClass )
+		
 		Map belongToMap = defaultDomainClass.getStaticPropertyValue(GrailsDomainClassProperty.BELONGS_TO, Map.class)
 		
 		def domainClassInstance
@@ -67,6 +76,16 @@ class DialogService {
             return returnMap
         }
     }
+	
+	/**
+	* Processes a form submission.
+	* The instance to be submitted is constructed based on params or the provided instance is used. This last option enables the controller to pre-prep the instance.
+	* 
+	* @param domainClass The domain class to be used
+	* @param params The parameters from the http request
+	* @param instance The domain object to be used (in lieu of creating one from params)
+	* @return a map that is ready to be rendered as a JSON message
+	*/
     	
 	def submit(domainClass,params,instance=null) {
 			def g=grailsApplication.mainContext.getBean('org.codehaus.groovy.grails.plugins.web.taglib.ApplicationTagLib')
@@ -78,6 +97,7 @@ class DialogService {
     		def domainClassInstance
     		if (instance) {
     			domainClassInstance=instance
+				action='updated'
     		} else {
 	    		if (params.id && params.id != 'null') {
 	    		    if (params.id.contains("_")){
@@ -94,23 +114,24 @@ class DialogService {
     		}
 			
             def successFlag=!domainClassInstance.hasErrors() && domainClassInstance.save(flush: true)
-            
-            def resultMessage
+			
+            def resultMessage=""
             def theErrorFields=[]
             if (successFlag) {
-            	domainClassInstance.save(flush: true)
-            
+            	domainClassInstance.save(flush: true)            
             	 def session = sessionFactory.getCurrentSession()
             	session.flush()
 				
             	resultMessage="${domainClassName} #${domainClassInstance.id} : ${domainClassInstance.toString()} ${action}" 
             } else {
-            	g.eachError(bean:domainClassInstance) {        		
-            		theErrorFields+=it.field
-            	}
+				if (domainClassInstance) {
+					domainClassInstance.errors.allErrors.each {						
+						theErrorFields+=it.field
+					}
+				}
             	resultMessage=g.renderErrors(bean:domainClassInstance)
             }
-    		                     
+    		
     		def result = [
     		              	success:successFlag,
     		              	message:resultMessage,
@@ -122,9 +143,21 @@ class DialogService {
              return res    		
     	}
 	
+	/**
+	* Delete a domain object
+	* The instance to be submitted is constructed based on params or the provided instance is used. This last option enables the controller to pre-prep the instance.
+	*
+	* @param domainClass The domain class to be used
+	* @param params The parameters from the http request
+	* @return a map that is ready to be rendered as a JSON message
+	*/
+	
+	
 	def delete(domainClass,params) {
 		def g=grailsApplication.mainContext.getBean('org.codehaus.groovy.grails.plugins.web.taglib.ApplicationTagLib')
 		def defaultDomainClass = new DefaultGrailsDomainClass( domainClass )
+		
+		
 		def domainPropertyName=defaultDomainClass.propertyName		
 		def domainClassName=defaultDomainClass.getName()
 	
@@ -159,7 +192,12 @@ class DialogService {
          def res=[result:result]
          return res    		
 	}
-
+	
+	/**
+	* Pretty print a XML document	
+	* @param inputXml The text to be processed
+	* @return Pretty XML
+	*/
 	def prettyPrint(String inputXml) {
 	     Source xmlInput = new StreamSource(new StringReader(inputXml));
 	     StreamResult xmlOutput = new StreamResult(new StringWriter());
@@ -173,5 +211,52 @@ class DialogService {
 	     return xmlOutput.getWriter().toString()
 	    	     
 	}
+	
+	/**
+	* Manage a join relationship. This is to accomodate the type of n:m relationship that the security plugin uses.
+	* This is ment to be invoked directly after the normal form submission
+	* @param params The request parameters
+	* @param instance The current domain instance to be used
+	* @param propertyName The 'property' that provides the related items from the target class
+	* @param joinClass The class that is used to maintain the join relationship. Needs to provide a create and a remove method
+	* @param targetClass The class that is on the other side of the n:m relationship
+	* @return Pretty XML
+	*/
+	
+	def manageJoin(params,instance,propertyName,joinClass,targetClass) {
+	
+		def newAuthorities=[]
+		if (params."${propertyName}") {
+			if (params."${propertyName}".class==java.lang.String) {
+				newAuthorities=[new java.lang.Long(params."${propertyName}")]
+			} else {
+				newAuthorities=params."${propertyName}".collect { return new java.lang.Long(it) }
+			}
+		}
+	
+		def currentAuthorities=[]
+		instance."${propertyName}".each { currentAuthority ->
+			currentAuthorities+=currentAuthority.id
+		}
+	   
+		// Add UserRole entries that are missing
+		
+		newAuthorities.each { newAuthority ->
+			if (!currentAuthorities.contains(newAuthority)) {
+				joinClass.create(instance, targetClass.get(newAuthority))
+			}
+		}
+	
+	
+		// Remove UserRole entries that are superfluous
+		
+		currentAuthorities.each { currentAuthority ->
+			if (!newAuthorities.contains(currentAuthority)) {
+				joinClass.remove(instance,targetClass.get(currentAuthority))
+			}
+		}
+		
+	}
+	
 
 }
