b2a8e9c4e6e763ecfd68047fb8cbce13e70578d9
Joost Horward <joost@open-t.nl>
2013-10-05 07:25:49 +0200

    Added JSON response to exception controller

--- grails-app/controllers/org/open_t/dialog/DialogExceptionController.groovy
+++ grails-app/controllers/org/open_t/dialog/DialogExceptionController.groovy
@@ -1,8 +1,10 @@
 package org.open_t.dialog
+import grails.converters.JSON;
 
 class DialogExceptionController {
 
 	def dialog() {
+        println "Request.accept: ${request.getHeader("Accept")}"
 		def title="Error handling error"
 		def msg=""
 		def exceptionCode=request.exception?.message
@@ -20,6 +22,17 @@ class DialogExceptionController {
 		} catch (Exception e) {
 			msg=e.message
 		}
-		[title:title,msg:msg]
+        if (request && request.getHeader("Accept") && request.getHeader("Accept").contains("application/json")) {
+            
+            
+            def result = [
+    		              	success:false,
+    		              	message:msg							
+    		              ]
+			def res=[result:result]
+            render res as JSON
+        } else {
+            [title:title,msg:msg]
+        }
 	}
 }
