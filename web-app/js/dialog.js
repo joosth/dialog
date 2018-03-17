fe68444a0860cff8fd62056bf4c916b4a7e2c108
Joost Horward <joost@open-t.nl>
2018-03-17 09:28:22 +0100

    Added getUrlParameter

--- web-app/js/dialog.js
+++ web-app/js/dialog.js
@@ -63,6 +63,27 @@ dialog.obj2ParamStr = function obj2ParamStr(params) {
 };
 
 /**
+ * https://stackoverflow.com/questions/19491336/get-url-parameter-jquery-or-how-to-get-query-string-values-in-js
+ * get named parameter from url
+ *
+ */
+
+dialog.getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+/**
  * Show a confirmation dialog
  */
 dialog.confirm = function confirm(message, title, callback, data) {
